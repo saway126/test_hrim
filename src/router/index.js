ee2d87cd70344c8f5ca1889052225ea2af89d68d
kks <kks@example.com>
2025-04-02 19:29:45 +0900

    [Feat] 회원 검색 기능 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '../components/LoginForm.vue'
 import SignUpForm from '../components/SignUpForm.vue'
 import AdminUserApprovalView from '../views/AdminUserApprovalView.vue'
+import AdminUserSearchView from '../views/AdminUserSearchView.vue'
+
 
 
 
 const routes = [
   { path: '/login', component: LoginForm },
   { path: '/signup', component: SignUpForm },
-  { path: '/admin/user-approval', name: 'admin-user-approval', component: AdminUserApprovalView }
+  { path: '/admin/user-approval', name: 'admin-user-approval', component: AdminUserApprovalView },
+  {
+    path: '/admin/user-search',
+    name: 'admin-user-search',
+    component: AdminUserSearchView
+  }
+  
  
 ]
 
