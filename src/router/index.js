712e1f9199b8a9b3ad1bb12f47016a2faa126493
kks <kks@example.com>
2025-04-02 21:31:08 +0900

    [Feat]HomeView 추가가

--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,26 @@ import SignUpForm from '../components/SignUpForm.vue'
 import AdminUserApprovalView from '../views/AdminUserApprovalView.vue'
 import AdminUserSearchView from '../views/AdminUserSearchView.vue'
 import AdminPermissionTransferView from '../views/AdminPermissionTransferView.vue'
-//import CompanyRegisterView from '../views/CompanyRegisterView.vue'
+// import CompanyRegisterView from '../views/CompanyRegisterView.vue'
 import AdminLoginView from '../views/AdminLoginView.vue'
 import FindIdView from '../views/FindIdView.vue'
 import ResetPasswordView from '../views/ResetPasswordView.vue'
 import ChangePasswordView from '../views/ChangePasswordView.vue'
-
-
-
-
+import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  { path: '/login', component: AdminLoginView }, // 하나만!
+  { path: '/', component: HomeView },
+  { path: '/login', component: AdminLoginView },
   { path: '/signup', component: SignUpForm },
   { path: '/admin/user-approval', component: AdminUserApprovalView },
   { path: '/admin/user-search', component: AdminUserSearchView },
   { path: '/admin/user-permission', component: AdminPermissionTransferView },
-  { path: '/login', component: AdminLoginView },
-  { path: '/signup', component: SignUpForm },
-  { path: '/find-id', component: FindIdView }, // ✅ 아이디 찾기 라우트 추가
+  { path: '/find-id', component: FindIdView },
   { path: '/reset-password', component: ResetPasswordView },
-  { path: '/change-password', component: ChangePasswordView }
-  // CompanyRegisterView는 추후 복구
+  { path: '/change-password', component: ChangePasswordView },
+  // { path: '/register/company', component: CompanyRegisterView }, // 만들었다면 사용
+  { path: '/register/employee', component: SignUpForm }
 ]
-
 
 const router = createRouter({
   history: createWebHistory(),
