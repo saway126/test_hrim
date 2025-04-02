5d039a2ce82f74f44a273afc2fbc596ce80b0eba
kks <kks@example.com>
2025-04-02 19:07:07 +0900

    [Feat] Upload 회원 승인기능 추가 및 불필요한 뷰 제거거

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '../components/LoginForm.vue'
 import SignUpForm from '../components/SignUpForm.vue'
+import AdminUserApprovalView from '../views/AdminUserApprovalView.vue'
 
 
 
 const routes = [
   { path: '/login', component: LoginForm },
   { path: '/signup', component: SignUpForm },
-
-  
+  { path: '/admin/user-approval', name: 'admin-user-approval', component: AdminUserApprovalView }
+ 
 ]
 
 const router = createRouter({
