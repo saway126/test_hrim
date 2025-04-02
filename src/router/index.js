96a4bbf15051a4334059df7d6a9757da21d9c218
kks <kks@example.com>
2025-04-02 21:11:34 +0900

    [Feat] 아이디 찾기기 기능 추가

--- src/router/index.js
+++ src/router/index.js
@@ -4,38 +4,26 @@ import SignUpForm from '../components/SignUpForm.vue'
 import AdminUserApprovalView from '../views/AdminUserApprovalView.vue'
 import AdminUserSearchView from '../views/AdminUserSearchView.vue'
 import AdminPermissionTransferView from '../views/AdminPermissionTransferView.vue'
-import CompanyRegisterView from '../views/CompanyRegisterView.vue'
-
-
+//import CompanyRegisterView from '../views/CompanyRegisterView.vue'
+import AdminLoginView from '../views/AdminLoginView.vue'
+import FindIdView from '../views/FindIdView.vue'
 
 
 
 
 
 const routes = [
-  { path: '/login', component: LoginForm },
+  { path: '/login', component: AdminLoginView }, // 하나만!
   { path: '/signup', component: SignUpForm },
-  { path: '/admin/user-approval', name: 'admin-user-approval', component: AdminUserApprovalView },
-  {
-    path: '/admin/user-search',
-    name: 'admin-user-search',
-    component: AdminUserSearchView
-  },
-  {
-    path: '/admin/user-permission',
-    name: 'admin-user-permission',
-    component: AdminPermissionTransferView
-  },
-  {
-    path: '/register/company',
-    name: 'register-company',
-    component: CompanyRegisterView
-  }
-  
-  
-  
- 
+  { path: '/admin/user-approval', component: AdminUserApprovalView },
+  { path: '/admin/user-search', component: AdminUserSearchView },
+  { path: '/admin/user-permission', component: AdminPermissionTransferView },
+  { path: '/login', component: AdminLoginView },
+  { path: '/signup', component: SignUpForm },
+  { path: '/find-id', component: FindIdView }, // ✅ 아이디 찾기 라우트 추가
+  // CompanyRegisterView는 추후 복구
 ]
+
 
 const router = createRouter({
   history: createWebHistory(),
