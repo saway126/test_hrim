8ee5ca386a95af2d74e8d4e0dddb25d0b6f3a094
kks <kks@example.com>
2025-04-02 18:45:13 +0900

    Feat 기본적 틀만 남김김

--- src/router/index.js
+++ src/router/index.js
@@ -2,30 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '../components/LoginForm.vue'
 import SignUpForm from '../components/SignUpForm.vue'
 
-// 관리자 상세 관련 분할 컴포넌트
-import AdminProfile from '../components/AdminProfile.vue'
-import AdminPermissions from '../components/AdminPermissions.vue'
-import AdminLogs from '../components/AdminLogs.vue'
-import AdminDeactivate from '../components/AdminDeactivate.vue'
-import AdminMgmtUpdateCycle from '../components/AdminMgmtUpdateCycle.vue'
-import AdminPartnerCycle from '../components/AdminPartnerCycle.vue'
-import ReportList from '../components/ReportList.vue'
-import ReportDetail from '../components/ReportDetail.vue'
-import EcoProductDetail from '../components/EcoProductDetail.vue'
+
 
 const routes = [
   { path: '/login', component: LoginForm },
   { path: '/signup', component: SignUpForm },
-  { path: '/admin/:id/profile', component: AdminProfile },
-  { path: '/admin/:id/permissions', component: AdminPermissions },
-  { path: '/admin/:id/logs', component: AdminLogs },
-  { path: '/admin/:id/deactivate', component: AdminDeactivate },
-  { path: '/admin/:id/mgmt-update-cycle', component: AdminMgmtUpdateCycle },
-  { path: '/admin/:id/partner-cycle', component: AdminPartnerCycle },
-  { path: '/report/list', component: ReportList },
-  { path: '/report/:id', component: ReportDetail },
-  { path: '/', redirect: '/login' },
-  { path: '/eco-product/:id', component: EcoProductDetail }
+
   
 ]
 
