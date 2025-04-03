56b1b93e8f33f105ce181b594081093f19eb761f
kks <kks@example.com>
2025-04-03 15:07:10 +0900

    [Refactor] 뷰 컴포넌트 디렉토리 정리리

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginForm from '../components/LoginForm.vue'
-import SignUpForm from '../components/BusinessCertModal.vue'
-import AdminUserApprovalView from '../views/AdminUserApprovalView.vue'
-import AdminUserSearchView from '../views/AdminUserSearchView.vue'
-import AdminPermissionTransferView from '../views/AdminPermissionTransferView.vue'
-import CompanyRegisterView from '../views/CompanyRegisterView.vue'
-import AdminLoginView from '../views/AdminLoginView.vue'
-import FindIdView from '../views/FindIdView.vue'
-import ResetPasswordView from '../views/ResetPasswordView.vue'
-import ChangePasswordView from '../views/ChangePasswordView.vue'
-import HomeView from '../views/HomeView.vue'
-import EmployeeRegisterView from '../views/EmployeeRegisterView.vue'
-import DashboardView from '../views/DashboardView.vue'
-import ActivityManageView from '../views/ActivityManageView.vue'
-import CampaignActivityView from '../views/CampaignActivityView.vue'
 
+// 로그인 및 인증 관련
+import AdminLoginView from '../views/admin/AdminLoginView.vue'
+import FindIdView from '../views/auth/FindIdView.vue'
+import ResetPasswordView from '../views/auth/ResetPasswordView.vue'
+import ChangePasswordView from '../views/auth/ChangePasswordView.vue'
+
+// 홈
+import HomeView from '../views/home/HomeView.vue'
+
+// 회원가입
+import CompanyRegisterView from '../views/register/CompanyRegisterView.vue'
+import EmployeeRegisterView from '../views/register/EmployeeRegisterView.vue'
+
+// 관리자 관련
+import AdminUserApprovalView from '../views/admin/AdminUserApprovalView.vue'
+import AdminUserSearchView from '../views/admin/AdminUserSearchView.vue'
+import AdminPermissionTransferView from '../views/admin/AdminPermissionTransferView.vue'
+
+// 활동 인증 관리
+import ActivityManageView from '../views/activity/ActivityManageView.vue'
+import CampaignActivityView from '../views/activity/CampaignActivityView.vue'
+
+// 대시보드
+import DashboardView from '../views/dashboard/DashboardView.vue'
+
+// 컴포넌트 (필요 시 모달이나 테이블만 import)
+import BusinessCertModal from '../views/admin/components/BusinessCertModal.vue'
+// import PermissionTable from '../views/admin/components/PermissionTable.vue'
 
 const routes = [
   { path: '/', component: HomeView },
+
+  // 로그인 & 인증
   { path: '/login', component: AdminLoginView },
-  { path: '/signup', component: SignUpForm },
-  { path: '/admin/user-approval', component: AdminUserApprovalView },
-  { path: '/admin/user-search', component: AdminUserSearchView },
-  { path: '/admin/user-permission', component: AdminPermissionTransferView },
   { path: '/find-id', component: FindIdView },
   { path: '/reset-password', component: ResetPasswordView },
   { path: '/change-password', component: ChangePasswordView },
-  { path: '/register/company', component: CompanyRegisterView }, // 만들었다면 사용
-  { path: '/register/employee', component: SignUpForm },
-  { path: '/register/employee', component: EmployeeRegisterView },
-  { path: '/dashboard', component: DashboardView },
-  { path: '/admin/activity-manage', name: 'activity-manage', component: ActivityManageView },
-  { path: '/admin/campaign-activity', name: 'CampaignActivity', component: CampaignActivityView },
 
+  // 관리자 기능
+  { path: '/admin/user-approval', component: AdminUserApprovalView },
+  { path: '/admin/user-search', component: AdminUserSearchView },
+  { path: '/admin/user-permission', component: AdminPermissionTransferView },
+
+  // 회원가입
+  { path: '/register/company', component: CompanyRegisterView },
+  { path: '/register/employee', component: EmployeeRegisterView },
+
+  // 활동 인증
+  { path: '/admin/activity-manage', component: ActivityManageView },
+  { path: '/admin/campaign-activity', component: CampaignActivityView },
+
+  // 대시보드
+  { path: '/dashboard', component: DashboardView },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
