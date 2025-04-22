163969d3ca9811527e2cfdec6e8a70e6f2c553f0
kks <skwka12346@gmail.com>
2025-04-22 20:35:34 +0900

    [feat]test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 import router from './router'
-import './assets/tailwind.css'
+import {createPinia} from 'pinia'
+import piniaPersistedstate from "pinia-plugin-persistedstate"
 
-createApp(App).use(router).mount('#app')
+// v-calendar
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
+
+const pinia = createPinia();
+pinia.use(piniaPersistedstate);
+
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.use(setupCalendar, {})
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // ✅ path 모듈 import
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // ✅ @ => src 폴더
-    }
-  }
+  server: {
+    proxy: {
+      // 프록시할 경로
+      "/api": {
+        // 대상 서버
+        target: "http://localhost:8080/",
+        // 대상 서버의 호스트 헤더 변경 여부
+        changeOrigin: true,
+        // '/api' 부분 제거
+        rewrite: (path) => path.replace(/^\/api/,""),
+      },
+    },
+  },
 })
